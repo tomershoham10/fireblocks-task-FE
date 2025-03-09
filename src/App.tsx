5e439198f735d5ca462759d3595ffe22a072e987
tomershoham10 <tomershoham10@gmail.com>
2025-03-09 21:46:56 +0200

    feat: implement task management dashboard with task completion functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-
-import './App.css'
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <section className="bg-[#F6F6F6] h-screen">
+      <Dashboard />
+    </section>
+  );
 }
 
-export default App
+export default App;
